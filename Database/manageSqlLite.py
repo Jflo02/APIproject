fe6681d3e799fd2746dfce23f0652550de1a5d8e
Nicolas Tahon <nicolas.tahon@epsi.fr>
2021-10-08 14:20:56 +0200

    ajout script creation de BDD

--- Database/manageSqlLite.py
+++ Database/manageSqlLite.py
@@ -4,10 +4,13 @@ import sqlite3
 class ManageSqlLite:
     def __init__(self, databaseName):
         self.sqlConnection = sqlite3.connect(databaseName)
-        self.cursor = self.sqlConnection.cursor()
+        self.sqlCursor = self.sqlConnection.cursor()
 
     def executeSqlCommande(self, sqlCmd):
         self.sqlCursor.execute(sqlCmd)
+
+    def executeSqlScript(self, sqlScript):
+        self.sqlCursor.executescript(sqlScript)
 
     def commmit(self):
         self.sqlConnection.commit()
--- Database/scriptCreation.py
+++ Database/scriptCreation.py
@@ -1,7 +1,10 @@
 from manageSqlLite import ManageSqlLite
 
 sql = ManageSqlLite("EasySav.db")
-sqlCreateTable=f"CREATE TABLE IF NOT EXISTS intervention"\
-                f"id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,"\
-                f""\
-                f""
+
+sqlCreateTable = open("scriptbddAPI.txt", "r")
+
+sql.executeSqlScript(sqlCreateTable.read())
+sqlCreateTable.close()
+sql.commmit()
+sql.closeConnection()
