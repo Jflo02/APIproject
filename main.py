8c819e8dc29e9aea1b489dadc720c4d747bc2bfe
flori <florimond2002@gmail.com>
2021-10-08 23:23:01 +0200

    mise à jour du script de création de bdd

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
+from flask import Flask
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = Flask(__name__)
 
 
-# Press the green button in the gutter to run the script.
+@app.route('/')
+def hello():
+    return "Bienvenue sur Easy Sav"
+
+
+@app.route('/helloWorld')
+def helloworld():
+    return "Hello world Easy Sav"
+
+
+@app.route('/getAllInterventions')
+def getAllInterventions():
+    return True
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run()
