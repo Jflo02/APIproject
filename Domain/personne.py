395313884dac3c84d8df8167205a0e2598b7e57a
Nicolas Tahon <nicolas.tahon@epsi.fr>
2021-10-09 11:10:20 +0200

    rectification abc Personne 2

--- Domain/personne.py
+++ Domain/personne.py
@@ -3,12 +3,6 @@ from abc import ABC
 
 
 class Personne(ABC):
-    def __init__(self, nom, prenom, numeroTelephone):
-        self.nom = nom
-        self.prenom = prenom
-        self.numeroTelephone = numeroTelephone
-        self.idPersonne = None
-
     @abc.abstractmethod
     def modifierInformations(self, ):
         pass
